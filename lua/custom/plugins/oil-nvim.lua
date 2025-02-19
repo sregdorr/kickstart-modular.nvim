114a8400744bfb5516328ca969ffeec88c5ee9bd
rrodgers <rrodgers0827@outlook.com>
2025-02-19 09:25:57 -0600

    fixup

--- lua/custom/plugins/oil-nvim.lua
+++ lua/custom/plugins/oil-nvim.lua
@@ -3,7 +3,11 @@ M = {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      columns = {
+        'icon',
+      },
+    },
     -- Optional dependencies
     -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
